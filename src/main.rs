cfbb0967607f2f3555f129222bf554448017a75c
ivanjermakov <ivanjermakov1@gmail.com>
2022-12-12 13:44:19 +0100

    Grammar: method call

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,8 @@ pub mod parser;
 
 fn main() {
     let source = r#"
-        a = 42
-        b = 12
+        'a'.print()
     "#;
-    let try_parsed = NoisParser::parse(Rule::declaration, source);
-    println!("{:?}", try_parsed)
+    let try_parsed = NoisParser::parse(Rule::file, source);
+    println!("{:#?}", try_parsed.unwrap())
 }
