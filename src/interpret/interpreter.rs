e8892d43fad39cc8f33ab76a01cf918eff60a6e2
ivanjermakov <ivanjermakov1@gmail.com>
2023-01-21 16:12:36 +0100

    Integration tests

--- src/interpret/interpreter.rs
+++ src/interpret/interpreter.rs
@@ -1,6 +1,5 @@
-use std::cell::RefCell;
+use std::cell::{RefCell, RefMut};
 use std::collections::HashMap;
-use std::process::exit;
 
 use colored::Colorize;
 use log::debug;
@@ -10,7 +9,10 @@ use crate::error::Error;
 use crate::interpret::context::{Context, Definition, Scope};
 use crate::interpret::evaluate::Evaluate;
 
-pub fn execute(block: AstPair<Block>, a_ctx: AstContext) {
+pub fn execute<F>(block: AstPair<Block>, a_ctx: AstContext, mut update_ctx: F)
+where
+    F: FnMut(&mut RefMut<Context>),
+{
     let ctx_cell = RefCell::new(Context::stdlib(a_ctx));
     let ctx = &mut ctx_cell.borrow_mut();
     let r_defs = block
@@ -23,7 +25,7 @@ pub fn execute(block: AstPair<Block>, a_ctx: AstContext) {
         Ok(ds) => ds.into_iter().flatten().collect::<HashMap<_, _>>(),
         Err(e) => {
             eprintln!("{}", format!("{}", e).red());
-            exit(1);
+            panic!();
         }
     };
     let identifier = Identifier::new("main");
@@ -31,15 +33,18 @@ pub fn execute(block: AstPair<Block>, a_ctx: AstContext) {
         .push(Scope::new("global".to_string()).with_definitions(block_defs));
     debug!("push scope @{}", &ctx.scope_stack.last().unwrap().name);
 
+    update_ctx(ctx);
+
     // TODO: pass CLI args as main parameter, e.g. main = args {}
     ctx.scope_stack
         .push(Scope::new(identifier.to_string()).with_arguments(Some(vec![])));
     debug!("push scope @{}", &ctx.scope_stack.last().unwrap().name);
+
     let (main_id, main) = match ctx.find_definition(&identifier) {
         Some(Definition::User(id, exp)) => (id, exp),
         _ => {
             eprintln!("{}", format!("'{}' not found", identifier).red());
-            exit(1)
+            panic!()
         }
     };
     let mut a = ctx.scope_stack.last_mut().unwrap();
@@ -49,7 +54,7 @@ pub fn execute(block: AstPair<Block>, a_ctx: AstContext) {
         Err(e) => {
             let err = Error::new_cause(e, main_id.1 .0, &main_id.0, &ctx.ast_context);
             eprintln!("{}", format!("{}", err).red());
-            exit(1);
+            panic!()
         }
     };
     debug!("pop scope @{}", &ctx.scope_stack.last().unwrap().name);
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ extern crate pest_derive;
 
 use std::fs::read_to_string;
 use std::io;
+use std::panic::set_hook;
 use std::path::PathBuf;
-use std::process::exit;
 
 use atty::Stream;
 use clap::Parser as p;
@@ -31,10 +31,12 @@ pub mod stdlib;
 pub mod util;
 
 fn main() {
+    set_hook(Box::new(|_| {}));
+
     if let Some(source) = piped_input() {
         let a_ctx = AstContext { input: source };
         let ast = parse_ast(&a_ctx);
-        execute(ast, a_ctx);
+        execute(ast, a_ctx, |_| {});
         return;
     }
 
@@ -66,7 +68,7 @@ fn main() {
             let source = read_source(path);
             let a_ctx = AstContext { input: source };
             let ast = parse_ast(&a_ctx);
-            execute(ast, a_ctx);
+            execute(ast, a_ctx, |_| {});
         }
     }
 }
@@ -78,7 +80,7 @@ pub fn parse_ast(a_ctx: &AstContext) -> AstPair<Block> {
         Ok(a) => a,
         Err(e) => {
             eprintln!("{}", format!("{}", e).red());
-            exit(1);
+            panic!()
         }
     }
 }
@@ -93,7 +95,7 @@ pub fn read_source(path: &String) -> String {
         Ok(s) => s,
         Err(e) => {
             eprintln!("{}", format!("unable to read file {}: {}", path, e).red());
-            exit(1)
+            panic!()
         }
     }
 }
@@ -109,4 +111,100 @@ pub fn piped_input() -> Option<String> {
             .collect::<Vec<_>>()
             .join("\n"),
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use std::cell::RefMut;
+    use std::collections::HashMap;
+    use std::fs::read_to_string;
+
+    use crate::ast::ast::{AstContext, AstPair};
+    use crate::error::Error;
+    use crate::interpret::context::{Context, Scope};
+    use crate::interpret::interpreter::execute;
+    use crate::interpret::value::Value;
+    use crate::parse_ast;
+    use crate::stdlib::lib::LibFunction;
+
+    fn run_file(name: &str) -> String {
+        static mut OUT: Vec<String> = vec![];
+
+        /// Override stdlib println function to collect all std output into a variable for further
+        /// assertions
+        struct TestPrintln;
+
+        impl LibFunction for TestPrintln {
+            fn name() -> String {
+                "println".to_string()
+            }
+
+            fn call(
+                args: &Vec<AstPair<Value>>,
+                _ctx: &mut RefMut<Context>,
+            ) -> Result<Value, Error> {
+                unsafe {
+                    OUT.push(format!(
+                        "{}",
+                        args.into_iter()
+                            .map(|a| a.1.to_string())
+                            .collect::<Vec<_>>()
+                            .join(" ")
+                    ));
+                }
+                Ok(Value::Unit)
+            }
+        }
+
+        let source = read_to_string(format!("data/{name}.no")).unwrap();
+        let a_ctx = AstContext {
+            input: source.to_string(),
+        };
+        let ast = parse_ast(&a_ctx);
+        execute(ast, a_ctx, |ctx| {
+            ctx.scope_stack.push(
+                Scope::new("test".to_string())
+                    .with_definitions(HashMap::from([TestPrintln::definition()])),
+            );
+        });
+        unsafe {
+            let res = OUT.clone().join("\n");
+            OUT = vec![];
+            res
+        }
+    }
+
+    #[test]
+    fn run_hello() {
+        let res = run_file("hello");
+        assert_eq!(res, format!("Hello, World!"))
+    }
+
+    #[test]
+    #[should_panic]
+    fn run_panic() {
+        run_file("panic");
+    }
+
+    #[test]
+    fn run_adding_lists() {
+        let res = run_file("adding_lists");
+        assert_eq!(
+            res,
+            format!(
+                r#"
+[1, 2]
+[1, 2, 3, 4]
+3
+[1, 2]
+[1, 2]
+[0, 1, 2, 3]
+[[0, 1], 2, 3]
+[0, 1, [2, 3]]
+[0, 1, 2, 3]
+"#
+            )
+                .trim()
+        )
+    }
 }
