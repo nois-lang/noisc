1f2e299d899537434ede0683cb4e253f7c80e2f7
ivanjermakov <ivanjermakov1@gmail.com>
2023-06-02 01:35:26 +0200

    Lexer: refactoring

--- src/lexer/lexer.ts
+++ src/lexer/lexer.ts
@@ -23,78 +23,106 @@ export const tokenize = (code: String): Token[] => {
     const tokens: Token[] = []
 
     while (chars.length !== 0) {
-        let codeLeft = code.slice(code.length - chars.length)
-
         if (isWhitespace(chars[0])) {
             chars.splice(0, 1)
             continue
         }
-
+        let codeLeft = code.slice(code.length - chars.length)
         let constToken = [...constTokenMap.values()].find(v => codeLeft.startsWith(v.value))
         if (constToken) {
             tokens.push(constToken)
             chars.splice(0, constToken.value.length)
             continue
         }
-
-        // TODO: floats
-        // TODO: sign
-        // TODO: scientific notation
-        if (isAlpha(chars[0])) {
-            const identifier: string[] = []
-            while (isAlpha(chars[0]) || isNumeric(chars[0])) {
-                identifier.push(chars[0])
-                chars.splice(0, 1)
-            }
-            tokens.push({type: 'identifier', value: identifier.join('')})
+        if (parseIdentifier(chars, tokens)) {
             continue
         }
-
-        if (isNumeric(chars[0])) {
-            const number: string[] = []
-            while (isNumeric(chars[0])) {
-                number.push(chars[0])
-                chars.splice(0, 1)
-            }
-            // TODO: verify literal
-            tokens.push({type: 'number', value: number.join('')})
+        if (parseNumberLiteral(chars, tokens)) {
             continue
         }
-
-        // TODO: escape characters
-        if (chars[0] === `'`) {
-            chars.splice(0, 1)
-            const charLiteral: string[] = []
-            while (chars[0] !== `'`) {
-                charLiteral.push(chars[0])
-                chars.splice(0, 1)
-            }
-            chars.splice(0, 1)
-            // TODO: verify literal
-            tokens.push({type: 'char', value: charLiteral.join('')})
+        if (parseCharLiteral(chars, tokens)) {
             continue
         }
-
-        // TODO: escape characters
-        if (chars[0] === '"') {
-            chars.splice(0, 1)
-            const stringLiteral: string[] = []
-            while (chars[0] !== '"') {
-                if (chars.length === 0) {
-                    throw Error('no matching `"`')
-                }
-                stringLiteral.push(chars[0])
-                chars.splice(0, 1)
-            }
-            chars.splice(0, 1)
-            // TODO: verify literal
-            tokens.push({type: 'string', value: stringLiteral.join('')})
+        if (parseStringLiteral(chars, tokens)) {
             continue
         }
 
         throw Error(`unknown token \`${chars[0]}\``)
     }
     return tokens
+}
+
+const parseIdentifier = (chars: string[], tokens: Token[]): boolean => {
+    if (isAlpha(chars[0])) {
+        const identifier: string[] = []
+        while (isAlpha(chars[0]) || isNumeric(chars[0])) {
+            identifier.push(chars[0])
+            chars.splice(0, 1)
+        }
+        tokens.push({type: 'identifier', value: identifier.join('')})
+        return true
+    }
+    return false
+}
+
+/**
+ *
+ * TODO: floats
+ * TODO: sign
+ * TODO: scientific notation
+ *
+ * @param chars
+ * @param tokens
+ */
+const parseNumberLiteral = (chars: string[], tokens: Token[]): boolean => {
+    if (isNumeric(chars[0])) {
+        const number: string[] = []
+        while (isNumeric(chars[0])) {
+            number.push(chars[0])
+            chars.splice(0, 1)
+        }
+        // TODO: verify literal
+        tokens.push({type: 'number', value: number.join('')})
+        return true
+    }
+    return false
+}
+
+const parseCharLiteral = (chars: string[], tokens: Token[]): boolean => {
+    // TODO: escape characters
+    if (chars[0] === `'`) {
+        chars.splice(0, 1)
+        const charLiteral: string[] = []
+        while (chars[0] !== `'`) {
+            charLiteral.push(chars[0])
+            chars.splice(0, 1)
+        }
+        chars.splice(0, 1)
+        // TODO: verify literal
+        tokens.push({type: 'char', value: charLiteral.join('')})
+        return true
+    }
+    return false
+}
+
+const parseStringLiteral = (chars: string[], tokens: Token[]): boolean => {
+    // TODO: escape characters
+    if (chars[0] === '"') {
+        chars.splice(0, 1)
+        const stringLiteral: string[] = []
+        while (chars[0] !== '"') {
+            if (chars.length === 0) {
+                throw Error('no matching `"`')
+            }
+            stringLiteral.push(chars[0])
+            chars.splice(0, 1)
+        }
+        chars.splice(0, 1)
+        // TODO: verify literal
+        tokens.push({type: 'string', value: stringLiteral.join('')})
+        return true
+    }
+    return false
 }
 
 const isWhitespace = (char: string): boolean => char === ' ' || char === '\t' || char === '\n' || char === '\r'
