34bace89be6c5a4cb06227b8cde5e5d771a6a046
ivanjermakov <ivanjermakov1@gmail.com>
2023-07-02 02:33:09 +0200

    Configurable output color

--- src/output.ts
+++ src/output.ts
@@ -1,8 +1,10 @@
+let colorOutput = true
+
 const format = {
     red: '\x1b[31m',
     yellow: '\x1b[33m',
     reset: '\x1b[0m'
 }
-export const red = (str: string): string => format.red + str + format.reset
+export const red = (str: string): string => colorOutput ? format.red + str + format.reset : str
 
-export const yellow = (str: string): string => format.yellow + str + format.reset
+export const yellow = (str: string): string => colorOutput ? format.yellow + str + format.reset : str
