e3f52cf1af5275a3e51a6bc539b0efb1646c2183
ivanjermakov <ivanjermakov1@gmail.com>
2023-09-03 22:22:27 +0200

    Semantic: check accessExpr

--- src/semantic/instance.ts
+++ src/semantic/instance.ts
@@ -1,36 +1,68 @@
 import { checkCallArgs, checkOperand } from '.'
-import { BinaryExpr } from '../ast/expr'
+import { BinaryExpr, OperandExpr } from '../ast/expr'
 import { CallOp } from '../ast/op'
-import { Operand } from '../ast/operand'
-import { FnDef, TraitDef } from '../ast/statement'
+import { Identifier, Operand } from '../ast/operand'
 import { Context } from '../scope'
-import { findTypeTraits, getImplTargetType } from '../scope/trait'
+import { getImplTargetType } from '../scope/trait'
 import { vidToString } from '../scope/util'
-import { VirtualIdentifierMatch, resolveVid } from '../scope/vid'
+import { resolveVid } from '../scope/vid'
 import { VirtualFnType, VirtualType, typeToVirtual, virtualTypeToString } from '../typecheck'
 import { resolveFnGenerics, resolveGenericsOverStructure, resolveType } from '../typecheck/generic'
+import { unknownType } from '../typecheck/type'
+import { allEqual } from '../util/array'
 import { notFoundError, semanticError } from './error'
 
 export const checkAccessExpr = (binaryExpr: BinaryExpr, ctx: Context): void => {
     const rOp = binaryExpr.rOperand
-    if (rOp.kind === 'identifier') {
-        checkFieldAccessExpr(binaryExpr, ctx)
+    if (rOp.kind === 'operand-expr' && rOp.operand.kind === 'identifier') {
+        binaryExpr.type = checkFieldAccessExpr(binaryExpr, ctx)
         return
     }
     if (rOp.kind === 'unary-expr' && rOp.unaryOp.kind === 'call-op') {
         binaryExpr.type = checkMethodCallExpr(binaryExpr.lOperand, rOp.operand, rOp.unaryOp, ctx)
         return
     }
-    if (rOp.kind === 'unary-expr' && rOp.unaryOp.kind === 'con-op') {
-        // TODO
-        return
-    }
-
     ctx.errors.push(semanticError(ctx, rOp, `expected field access or method call, got ${rOp.kind}`))
 }
 
-const checkFieldAccessExpr = (binaryExpr: BinaryExpr, ctx: Context): void => {
-    // TODO
+const checkFieldAccessExpr = (binaryExpr: BinaryExpr, ctx: Context): VirtualType | undefined => {
+    const lOp = binaryExpr.lOperand
+    const rOp = (<Identifier>(<OperandExpr>binaryExpr.rOperand).operand)
+    checkOperand(lOp, ctx)
+    // TODO: make sure no type args specified; check other identifier uses also
+    if (rOp.scope.length > 0) {
+        ctx.errors.push(semanticError(ctx, rOp, `expected field name`))
+        return
+    }
+    if (!lOp.type || lOp.type?.kind !== 'vid-type') {
+        ctx.errors.push(semanticError(ctx, rOp, `expected variant type, got ${lOp.type?.kind ?? unknownType.kind}`))
+        return
+    }
+    const typeVid = lOp.type.identifier
+    const typeRef = resolveVid(typeVid, ctx, ['type-def'])
+    if (!typeRef || typeRef.def.kind !== 'type-def') {
+        ctx.errors.push(notFoundError(ctx, lOp, vidToString(typeVid), 'type'))
+        return
+    }
+    const typeDef = typeRef.def
+    const fieldName = rOp.name.value
+    // check that every type variant has such field
+    if (!(typeDef.variants.length > 0 && typeDef.variants.every(v => v.fieldDefs.find(f => f.name.value === fieldName)))) {
+        ctx.errors.push(semanticError(ctx, rOp, `field \`${fieldName}\` is not defined in all variants of type \`${vidToString(typeRef.vid)}`))
+        return
+    }
+    // if field is defined in multiple variants, make sure their type is equal
+    const typeCandidates = typeDef.variants
+        .flatMap(v => v.fieldDefs.find(f => f.name.value === fieldName)?.fieldType ?? [])
+        .map(t => typeToVirtual(t, ctx))
+    // TODO: probably there is a better way to compare type equality
+    if (!allEqual(typeCandidates.map(virtualTypeToString))) {
+        ctx.errors.push(semanticError(ctx, rOp, `field \`${fieldName}\` of \`${vidToString(typeRef.vid)}\` variants must be of the same type to be accessed`))
+        return
+    }
+    // Resolve typeDef generics
+    const fieldType = typeCandidates[0]
+    return fieldType
 }
 
 const checkMethodCallExpr = (lOperand: Operand, rOperand: Operand, callOp: CallOp, ctx: Context): VirtualType | undefined => {
--- src/util/array.ts
+++ src/util/array.ts
@@ -1,1 +1,17 @@
 export const range = (from: number, to: number): number[] => new Array(to - from).fill(0).map((_, i) => i + from)
+
+/**
+ * Check wether every element in the array is equal using provided comparator function
+ * Will only check first element against every other, relying on cmp associativity (if cmp(a, b) === cmp(b, c), then
+ * cmp(a, c) === true)
+ */
+export const allEqual = <T>(arr: T[], cmp: (a: T, b: T) => boolean = (a, b) => a === b): boolean => {
+    if (arr.length === 0) return true
+    const first = arr[0]
+    for (let i = 1; i < arr.length; i++) {
+        if (!cmp(first, arr[i])) {
+            return false
+        }
+    }
+    return true
+}
