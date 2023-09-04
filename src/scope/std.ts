f2a41efbf525c6e3a36ba257541f72c48018aa3f
ivanjermakov <ivanjermakov1@gmail.com>
2023-09-04 22:06:26 +0200

    Std: std::panic, std::never

--- src/scope/std.ts
+++ src/scope/std.ts
@@ -3,17 +3,23 @@ import { VirtualIdentifier } from './vid'
 
 export const defaultImportedVids: VirtualIdentifier[] = [
     'std::unit::Unit',
+    'std::never::Never',
     'std::num::Num',
     'std::float::Float',
     'std::int::Int',
     'std::string::String',
     'std::char::Char',
     'std::list::List',
+
+    'std::panic::todo',
+    'std::panic::never',
+
     'std::math',
     'std::op::Add',
     'std::op::Sub',
     'std::op::Mult',
     'std::op::Div',
+
     'std::io',
     'std::io::println',
     'std::option::Option',
