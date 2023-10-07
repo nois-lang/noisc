89a5afb5bf3a9a5f475800514b403b3e9d19d100
ivanjermakov <ivanjermakov1@gmail.com>
2023-10-08 01:12:58 +0200

    Semantic: complete `typeNames`

--- src/semantic/type-def.ts
+++ src/semantic/type-def.ts
@@ -3,15 +3,17 @@ import { Type } from "../ast/type";
 export const typeNames = (type: Type): string[] => {
     switch (type.kind) {
         case 'identifier': {
-            return [type.name.value, ...type.typeArgs.flatMap(a => typeNames(a))]
+            return [type.name.value, ...type.typeArgs.flatMap(t => typeNames(t))]
         }
         case 'fn-type': {
-            // TODO: include fn types in the list of names
-            return []
+            return [
+                ...type.generics.flatMap(g => [g.name.value, ...g.bounds.flatMap(typeNames)]),
+                ...type.paramTypes.flatMap(typeNames),
+                ...typeNames(type.returnType)
+            ]
         }
         case 'type-bounds': {
-            // TODO: include type bounds in the list of names
-            return []
+            return type.bounds.flatMap(typeNames)
         }
     }
 }
