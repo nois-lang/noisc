5d26b02bb1ef39f6ae4524e2cb7a16e5bc407c53
ivanjermakov <ivanjermakov1@gmail.com>
2024-01-19 00:11:16 +0100

    Std: impl `Num` for `Float` & `Int`

--- src/std/int.js
+++ src/std/int.js
@@ -1,8 +1,62 @@
 /**
  * @param {Int} a
+ * @returns {Int}
+ */
+function negInt(a) {
+    return -a.value
+}
+
+/**
+ * @param {Int} a
+ * @param {Int} b
+ * @returns {Int}
+ */
+function absInt(a) {
+    return Math.abs(a.value)
+}
+
+/**
+ * @param {Int} a
  * @param {Int} b
  * @returns {Int}
  */
 function addInt(a, b) {
     return a.value + b.value
 }
+
+/**
+ * @param {Int} a
+ * @param {Int} b
+ * @returns {Int}
+ */
+function subInt(a, b) {
+    return a.value - b.value
+}
+
+/**
+ * @param {Int} a
+ * @param {Int} b
+ * @returns {Int}
+ */
+function multInt(a, b) {
+    return a.value * b.value
+}
+
+/**
+ * @param {Int} a
+ * @param {Int} b
+ * @returns {Int}
+ */
+function divInt(a, b) {
+    return a.value / b.value
+}
+
+/**
+ * @param {Int} a
+ * @param {Int} b
+ * @returns {Int}
+ */
+function expInt(a, b) {
+    return a.value ** b.value
+}
+
