4613c9a7a326de7b09e129c21a45b2f77b13fc97
ivanjermakov <ivanjermakov1@gmail.com>
2024-01-19 03:52:09 +0100

    Corrent location reporting with big line numbers

--- src/location.ts
+++ src/location.ts
@@ -29,16 +29,21 @@ export const prettyLineAt = (range: LocationRange, source: Source): string => {
     const start = indexToLocation(range.start, source)
     const end = indexToLocation(range.end, source)
     if (!start || !end) return '<outside of a file>'
-    const linePad = `${(start.line + 1).toString()} | `.padStart(6)
-    const pad = '| '.padStart(6)
+    const lineNumSize = start.line.toString().length
+    const padSize = Math.max(6, 3 + lineNumSize)
+    const linePad = `${(start.line + 1).toString()} | `.padStart(padSize)
+    const pad = '| '.padStart(padSize)
     const sourceLine = source.code.split('\n')[start.line]
     const line = linePad + sourceLine
     // TODO: multiline highlight
     // why special logic is needed when token is near EOL?
-    const highlightLen =
+    const highlightEnd =
         end.column === sourceLine.length
-            ? sourceLine.length - start.column
-            : (start.line === end.line ? end.column + 1 : sourceLine.length) - start.column
+            ? sourceLine.length
+            : start.line === end.line
+              ? end.column + 1
+              : sourceLine.length
+    const highlightLen = highlightEnd - start.column
     const highlight = pad + ' '.repeat(start.column) + '^'.repeat(highlightLen)
     return [pad, line, highlight].join('\n')
 }
