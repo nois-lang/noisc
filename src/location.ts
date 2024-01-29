81f87646e5b35603b06f99d2fa6b8104b3f1b69f
ivanjermakov <ivanjermakov1@gmail.com>
2024-01-29 23:10:06 +0100

    Multiline reporting

--- src/location.ts
+++ src/location.ts
@@ -31,21 +31,30 @@ export const prettyLineAt = (range: LocationRange, source: Source): string => {
     if (!start || !end) return '<outside of a file>'
     const lineNumSize = start.line.toString().length
     const padSize = Math.max(6, 3 + lineNumSize)
-    const linePad = `${(start.line + 1).toString()} | `.padStart(padSize)
     const pad = '| '.padStart(padSize)
-    const sourceLine = source.code.split('\n')[start.line]
-    const line = linePad + sourceLine
-    // TODO: multiline highlight
-    // why special logic is needed when token is near EOL?
-    const highlightEnd =
-        end.column === sourceLine.length
-            ? sourceLine.length
-            : start.line === end.line
-              ? end.column + 1
-              : sourceLine.length
-    const highlightLen = highlightEnd - start.column
-    const highlight = pad + ' '.repeat(start.column) + '^'.repeat(highlightLen)
-    return [pad, line, highlight].join('\n')
+    const linePad = `${(start.line + 1).toString()} | `.padStart(padSize)
+    if (start.line === end.line) {
+        const sourceLine = source.code.split('\n')[start.line]
+        const line = linePad + sourceLine
+        const lastLineColumn = sourceLine.length - 1
+        const highlightEnd = end.column === sourceLine.length && start.line !== end.line ? end.column : lastLineColumn
+        const highlightLen = highlightEnd + 1 - start.column
+        const highlight = pad + ' '.repeat(start.column) + '^'.repeat(highlightLen)
+        return [pad, line, highlight].join('\n')
+    } else {
+        const lines = source.code.split('\n').slice(start.line, end.line + 1)
+        const first = pad + '-'.repeat(start.column) + 'v'.repeat(lines[0].length - start.column)
+        const last = pad + '-'.repeat(end.column - 1) + '^'.repeat(lines.at(-1)!.length - end.column + 1)
+        const linesErr = lines
+            .map((sourceLine, i) => {
+                let lineNum = start.line + i + 1
+                const isPartialHighlight = i === 0 || i === lines.length - 1
+                const linePad = `${lineNum.toString()} |${isPartialHighlight ? ' ' : '>'}`.padStart(padSize)
+                return linePad + sourceLine
+            })
+            .join('\n')
+        return [first, linesErr, last].join('\n')
+    }
 }
 
 export const locationToString = (location: Location): string => `${location.line + 1}:${location.column + 1}`
