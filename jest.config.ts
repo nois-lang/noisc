4a1ba7607b2ae35fc4e0443e7b5882581f07c72d
ivanjermakov <ivanjermakov1@gmail.com>
2024-01-31 02:18:01 +0100

    Only use bun for build/run/test

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-    moduleDirectories: ['node_modules', '<rootDir>'],
-    testEnvironment: 'node',
-    transform: {
-        '^.+\\.(t|j)sx?$': '@swc/jest'
-    }
-}
