12f2c848c32cb63832ed796df5a58a9951bb422c
ivanjermakov <ivanjermakov1@gmail.com>
2024-08-03 18:53:25 +0200

    Remove `Num`; operators are `Int` specific

--- src/semantic/op.ts
+++ src/semantic/op.ts
@@ -3,11 +3,12 @@ import { Identifier } from '../ast/operand'
 import { idFromString } from '../scope'
 
 export const operatorImplMap: Map<AstNodeKind, Identifier> = new Map([
-    ['add-op', idFromString('std::num::Num::add')],
-    ['sub-op', idFromString('std::num::Num::sub')],
-    ['mult-op', idFromString('std::num::Num::mult')],
-    ['div-op', idFromString('std::num::Num::div')],
-    ['exp-op', idFromString('std::num::Num::exp')],
+    ['add-op', idFromString('std::int::Int::add')],
+    ['sub-op', idFromString('std::int::Int::sub')],
+    ['mult-op', idFromString('std::int::Int::mult')],
+    ['div-op', idFromString('std::int::Int::div')],
+    ['exp-op', idFromString('std::int::Int::exp')],
+    ['mod-op', idFromString('std::int::Int::mod')],
     ['eq-op', idFromString('std::eq::Eq::eq')],
     ['ne-op', idFromString('std::eq::Eq::ne')],
     ['ge-op', idFromString('std::ord::Ord::ge')],
@@ -15,6 +16,5 @@ export const operatorImplMap: Map<AstNodeKind, Identifier> = new Map([
     ['gt-op', idFromString('std::ord::Ord::gt')],
     ['lt-op', idFromString('std::ord::Ord::lt')],
     ['and-op', idFromString('std::bool::Bool::and')],
-    ['or-op', idFromString('std::bool::Bool::or')],
-    ['mod-op', idFromString('std::int::Int::mod')]
+    ['or-op', idFromString('std::bool::Bool::or')]
 ])
