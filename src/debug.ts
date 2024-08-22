67b1f64f3c3769bb91eaa9ce476623f28c580bf8
ivanjermakov <ivanjermakov1@gmail.com>
2024-08-23 00:02:43 +0200

    Focused properties in `debugAst`

--- src/debug.ts
+++ src/debug.ts
@@ -1,27 +1,44 @@
 import { AstNode } from './ast'
 import { inferredTypeToString } from './typecheck'
+import { ExtractKeys } from './util/type'
 
-export const debugAst = (node: AstNode, depth = 0): any => {
-    if (depth > 50) return '@rec'
-    if (typeof node !== 'object') return node
-    return Object.fromEntries(
+export const debugAst = (node: AstNode, focusKinds: ExtractKeys<AstNode>[] = ['kind', 'type'], depth = 0): any => {
+    if (depth > 10) return '@rec'
+
+    const o = Object.fromEntries(
         Object.entries(node)
             .filter(([p]) => !['parseNode', 'source'].includes(p))
-            .filter(([, v]) => !(Array.isArray(v) && v.length === 0))
             .map(([p, v]) => {
                 if (p === 'type') {
-                    return [p, inferredTypeToString(v)]
+                    if (focusKinds.includes(<any>p)) {
+                        return [p, inferredTypeToString(v)]
+                    } else {
+                        return undefined
+                    }
                 }
                 if (p === 'def' || p === 'typeDef') {
-                    return [p, v.kind]
+                    if (focusKinds.includes(<any>p)) {
+                        return [p, v.kind]
+                    } else {
+                        return undefined
+                    }
                 }
                 if (Array.isArray(v)) {
-                    return [p, v.map(i => debugAst(i, depth + 1))]
+                    const items = v.map(i => debugAst(i, focusKinds, depth + 1)).filter(i => i !== undefined)
+                    return [p, items]
                 }
                 if (typeof v === 'object' && 'parseNode' in v) {
-                    return [p, debugAst(v, depth + 1)]
+                    return [p, debugAst(v, focusKinds, depth + 1)]
                 }
-                return [p, v]
+                if (focusKinds.includes(<any>p)) {
+                    return [p, v]
+                }
+                return undefined
             })
+            .filter(t => t !== undefined)
+            .map(t => t!)
+            .filter(([, v]) => v !== undefined && !(Array.isArray(v) && v.length === 0))
     )
+    if (Object.keys(o).length === 0) return undefined
+    return o
 }
